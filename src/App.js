56ba3583a56d2eb7a7408ee0a77ef2688ab58f63
Juned Mansuri <mansurijuned111@gmail.com>
2021-04-23 15:59:13 +0530

    param fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
 import Home from './containers/Home';
 import Login from './containers/Login';
 import Signup from './containers/Signup';
-
 import { Provider } from 'react-redux';
 import store from './store';
-
 import Layout from './hocs/Layout';
 
 const App = () => (
